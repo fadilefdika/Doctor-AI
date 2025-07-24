232cbe6cb8a3794334e43686aa7f1b9ae7377eed
uvenven <fadilefd1102@gmail.com>
2025-07-24 16:07:52 +0700

    membuat fitur login dan register

--- Backend/main.py
+++ Backend/main.py
@@ -1,18 +1,16 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from utils.LLM_utils import ask_llm
+from routers import auth, chat, doctor
 
 app = FastAPI()
 
+app.include_router(auth.router)
+app.include_router(chat.router)
+app.include_router(doctor.router)
+
 class ChatRequest(BaseModel):
     message: str
-
-@app.post("/chat")
-def chat(request: ChatRequest):
-    reply = ask_llm(request.message)
-    return {"response": reply}
-
-
+    
 @app.get("/")
 def read_root():
     return {"welcome to doctor AI"}
