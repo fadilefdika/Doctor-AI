0dfbc5205304c096d44eae5884c095693a2223a8
uvenven <fadilefd1102@gmail.com>
2025-07-25 09:33:03 +0700

    resolve

--- Backend/services/openai_llm.py
+++ Backend/services/openai_llm.py
@@ -1,6 +1,6 @@
 from openai import OpenAI
 
-client = OpenAI(api_key="YOUR_OPENAI_API_KEY")  # bisa juga pakai env var: api_key=os.getenv("OPENAI_API_KEY")
+client = OpenAI(api_key="OPENAI_API_KEY")  # bisa juga pakai env var: api_key=os.getenv("OPENAI_API_KEY")
 
 def ask_gpt(*messages: dict) -> str:
     response = client.chat.completions.create(
